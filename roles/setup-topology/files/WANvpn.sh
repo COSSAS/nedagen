8104b515656afacf9f8f14375ac84f7f36356f8f
Dennis van Wijk <dennis.vanwijk@tno.nl>
2022-01-18 22:11:18 +0100

    updates on capture and traffic generator

---
+++ roles/setup-topology/files/WANvpn.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+first='yes'
+
+while true; do
+    while true; do
+        set $(dd if=/dev/urandom bs=4 count=1 2>/dev/null | od -An -tu1)
+        ip_address=193.166.$3.$4
+        if [ $4 -ne 0 ] && [ $4 -ne 1 ] && [ $4 -ne 255 ]
+        then
+            break
+        fi
+    done
+    echo $ip_address
+
+    ip addr add $ip_address/16 dev eth1
+
+    if [ $first == 'yes' ]; then
+        ip route delete default
+        ip route add 0.0.0.0/0 via 193.166.0.1 dev eth1
+        echo "nameserver 5.5.5.5" > /etc/resolv.conf
+        first='no'
+    fi
+
+    sleep 2
+
+    # do shit
+
+    ip addr del $ip_address/16 dev eth1
+
+    sleep 2
+
+done
---
+++ roles/setup-topology/files/WANweb.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+first='yes'
+
+while true; do
+    while true; do
+        set $(dd if=/dev/urandom bs=4 count=1 2>/dev/null | od -An -tu1)
+        ip_address=96.$2.$3.$4
+        if [ $4 -ne 0 ] && [ $4 -ne 1 ] && [ $4 -ne 255 ]
+        then
+            break
+        fi
+    done
+    echo $ip_address
+
+    ip addr add $ip_address/8 dev eth1
+
+    if [ $first == 'yes' ]; then
+        ip route delete default
+        ip route add 0.0.0.0/0 via 96.0.0.1 dev eth1
+        echo "nameserver 5.5.5.5" > /etc/resolv.conf
+        first='no'
+    fi
+
+    curl LANsite.dev
+    sleep 2
+    ip addr del $ip_address/8 dev eth1
+    sleep 2
+done
