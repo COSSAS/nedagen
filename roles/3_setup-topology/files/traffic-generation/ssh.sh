7890e4758a9d7f317895b8d87c48ce0bc2f5838c
Jeroen van Saane <jeroen.vansaane@tno.nl>
2022-01-28 12:03:51 +0100

    Fully Working ALS Profiles + Cronjobs + ALS_script

--- roles/3_setup-topology/files/traffic-generation/ssh.sh
+++ roles/3_setup-topology/files/traffic-generation/ssh.sh
@@ -2,13 +2,15 @@
 
 ssh_weight=$1
 
-sshpass -p sshuserpassword ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  sshuser@172.16.0.3
+
 
 while true
 do
+    sshpass -p sshuserpassword ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  sshuser@172.16.0.3 << EOF
     dd if=/dev/random of=random.stuff bs=1M count=2
     rm random.stuff
-    sleep $ssh_weight
+EOF
+    sleep $((20*$ssh_weight))
     
 done
 
